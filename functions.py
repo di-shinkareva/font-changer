a716e80b232e59dcb89f99a4077c2f678edc142e
di-shinkareva <shinkareva.dr@mail.ru>
2024-11-27 13:18:45 +0300

    добавила функцию которая: Изменяет шрифт, размер шрифта и межстрочный интервал в документе.

--- functions.py
+++ functions.py
@@ -3,8 +3,14 @@
 # TODO - import <зависимость из задания>
 
 
-def func1(n):
-    """Функция 1"""
+def change_font_and_spacing(doc_path):
+    """Изменяет шрифт, размер шрифта и межстрочный интервал в документе."""
+    doc = Document (doc_path)
+    #размер шрифта в пунктах 
+for paragraph in doc.paragraphs:  
+    for run in doc.paragraphs:
+        run.font.name = 'Times New Roman'
+        run.font.size = 14
     pass
 
 
