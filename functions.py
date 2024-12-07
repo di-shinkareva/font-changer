52adfe87484397c420f5d845e5f63c266b5e66e6
di-shinkareva <shinkareva.dr@mail.ru>
2024-12-07 21:29:31 +0300

    шаг 5: установила шрифт

--- functions.py
+++ functions.py
@@ -9,3 +9,7 @@ from docx.oxml import parse_xml # type: ignore # Для обработки
 межстрочного интервала # type: ignore
 from docx.oxml.ns import nsdecls # type: ignore # Для работы с
 пространством имен XML # type: ignore # type: ignore
+
+# Устанавливаем шрифт
+run._element.rPr.rFonts.set(qn('W:eastAsia'), # type: ignore
+'Times New Roman')
