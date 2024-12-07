ba8e88f0a0b2d22949da0328df4aa5a6b641b71c
IvanovaVictoria7 <ivanovavi1111@mail.ru>
2024-12-07 21:36:31 +0300

    Шаг 9:исправила ошибку

--- functions.py
+++ functions.py
@@ -10,15 +10,10 @@ def modify_docx(file_path):
                 run.font.name = 'Times New Roman'  # Устанавливаем шрифт
                 run._element.rPr.rFonts.set(qn('w:eastAsia'), 'Times New Roman') # type: ignore
                 # Сохраняем изменения в новый файл
-        output_path = f"modified_{os.path.basename(file_path)}"
+        output_path = f"modified_{os.path.basename(file_path)}" # type: ignore
         doc.save(output_path)
         print(f"Документ сохранён: {output_path}")
 
-    except Exception as e:
+     except Exception as e:
         # Если возникает ошибка, выводим сообщение
-        print(f"Ошибка при обработке файла {file_path}: {e}")
-
-
-
-
-
+        print(f"Ошибка при обработке файла {file_path}: {e}") # type: ignore
