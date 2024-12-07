159ab6d28db98dc1c79e77cfcdeb71b79abcb7f1
IvanovaVictoria7 <ivanovavi1111@mail.ru>
2024-12-07 21:16:53 +0300

    Шаг 3:Открыла файл в .docx

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 #Функция для изменения параметров текста в документе 
 def modify_docx(file_path):
     try:
-        
+        #Окрываем .docx файл
+        doc=Document(file_path) # type: ignore
+
