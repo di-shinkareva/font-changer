4102c3963116ce503f6db568093e5914fe22e432
IvanovaVictoria7 <ivanovavi1111@mail.ru>
2024-12-07 21:25:44 +0300

    Шаг 4:прошлась по каждому абзацу и настроила текст внутри абзаца

--- functions.py
+++ functions.py
@@ -1,6 +1,15 @@
 #Функция для изменения параметров текста в документе 
 def modify_docx(file_path):
-    try:
+     try :
         #Окрываем .docx файл
         doc=Document(file_path) # type: ignore
+        # Проходим по каждому абзацу документа
+        for paragraph in doc.paragraphs:
+            # Настраиваем текст внутри абзаца
+            for run in paragraph.runs:
+                run.font.name = 'Times New Roman'  # Устанавливаем шрифт
+                run._element.rPr.rFonts.set(qn('w:eastAsia'), 'Times New Roman') # type: ignore
+
+
+
 
