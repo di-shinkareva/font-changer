d9b92d2c5880be826acca72adf79f091e839a3ba
di-shinkareva <shinkareva.dr@mail.ru>
2024-12-07 20:54:34 +0300

    первый шаг:добавила функции

--- functions.py
+++ functions.py
@@ -1,19 +1,11 @@
-## functions модуль для импорта функций для работы
-
-# TODO - import <зависимость из задания>
-
-
-def change_font_and_spacing(doc_path):
-    """Изменяет шрифт, размер шрифта и межстрочный интервал в документе."""
-    doc = Document (doc_path)
-    #размер шрифта в пунктах 
-for paragraph in doc.paragraphs:  
-    for run in doc.paragraphs:
-        run.font.name = 'Times New Roman'
-        run.font.size = 14
-    pass
-
-
-def func2(n):
-    """Функция 2"""
-    pass
+import os # Для работы с файловой системой
+from docx import Document # type: ignore # Для работы с
+документами .docx # type: ignore
+from docx.shared import pt # type: ignore # Для задания
+размера шрифта # type: ignore
+from docx.oxml.ns import qn # type: ignore #Для работы с XML-
+структурой документа # type: ignore
+from docx.oxml import parse_xml # type: ignore # Для обработки
+межстрочного интервала # type: ignore
+from docx.oxml.ns import nsdecls # type: ignore # Для работы с
+пространством имен XML # type: ignore # type: ignore
